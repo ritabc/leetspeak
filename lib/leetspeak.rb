8a0fc8e5c7ade82b159ee13e8605b3c784e886b6
Rita Bennett-Chew and Will Swanson <rita@>
2018-06-25 13:13:11 -0700

    add passing test 2

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,6 +1,6 @@
 class String
   def leetspeak
-    # words_array = self.split(" ")
+    words_array = self.split(" ")
     # words_array.each do |word|
     #   letters_array = word.split("")
     #   letters_array.each do |letter|
