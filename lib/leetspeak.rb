df64b8cda9eaa68698e4c29e2204b935203f5df3
Rita Bennett-Chew and Will Swanson <rita@>
2018-06-25 13:27:13 -0700

    add test 3 passing and reapir other tests

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,10 +1,19 @@
 class String
   def leetspeak
     words_array = self.split(" ")
-    # words_array.each do |word|
-    #   letters_array = word.split("")
-    #   letters_array.each do |letter|
-    #   end
-    # end
+    word_output_array = []
+    words_array.each do |word|
+      letters_array = word.split("")
+      letters_output_array = []
+      letters_array.each do |letter|
+        if letter.downcase === "e"
+          letters_output_array.push("3")
+        else
+          letters_output_array.push(letter)
+        end
+      end
+      word_output_array.push(letters_output_array.join(""))
+    end
+    word_output_array
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -6,11 +6,11 @@ describe('String#leetspeak') do
     expect("happy".leetspeak).to(eq(["happy"]))
   end
   it("takes a phrase or collection of sentences and breaks them apart by spaces") do
-    expect("the happy elephant".leetspeak).to(eq(["the", "happy", "elephant"]))
+    expect("the happy elephant".leetspeak).to(eq(["th3", "happy", "3l3phant"]))
   end
-  # it("replaces every 'e' with '3'") do
-  #   expect("elephant".leetspeak).to(eq("3l3phant"))
-  # end
+  it("replaces every 'e' with '3'") do
+    expect("elephant".leetspeak).to(eq(["3l3phant"]))
+  end
   # it("replaces every (capital) 'I' in a string with a '1'") do
   #   expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
   # end
